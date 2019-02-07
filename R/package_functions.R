eb16f646e89b53023a258653e84423a585837c39
alexhubbardOD <alex.hubbard@opendoor.com>
2019-02-06 22:24:53 -0800

    fixed

--- R/package_functions.R
+++ R/package_functions.R
@@ -752,7 +752,7 @@ ms_dcf_filter = function(y, model, plot = F){
                            dim = dim(sp$P0)))
     sp2 = SSmodel_ms(model$coef, yy_s[eval(parse(text = model$panelID)) == i, ], model$n_states, model$ms_var, model$panelID, model$timeID, init = init)
     ans2 = kim_filter(sp2$B0, sp2$P0, sp2$At, sp2$Dt, sp2$Ft, sp2$Ht, sp2$Qt, sp2$Rt, sp2$Tr_mat, yti)
-    if(any(is.na(ans$B_tt))){
+    if(any(is.na(ans2$B_tt))){
       ans = kim_filter(sp$B0, sp$P0, sp$At, sp$Dt, sp$Ft, sp$Ht, sp$Qt, sp$Rt, sp$Tr_mat, yti)
     }else{
       ans = ans2
